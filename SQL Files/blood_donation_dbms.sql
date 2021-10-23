dc0de00d7a3489690e0cc47687c736c6161c793e
hritika1025 <hritika.cse29@gmail.com>
2021-10-23 11:32:59 +0530

    sql files and app.py  added

---
+++ SQL Files/blood_donation_dbms.sql
@@ -0,0 +1,104 @@
+CREATE DATABASE IF NOT EXISTS blood_donation_dbms;
+USE blood_donation_dbms;
+CREATE TABLE IF NOT EXISTS  `Blood_bank` (
+           `Username` varchar(100) NOT NULL,
+           `Password` varchar(100) NOT NULL,
+           `License_Number` varchar(100) NOT NULL,
+           `Blood_bank_name` varchar(100) NOT NULL,
+           `Owner_name` varchar(100) NOT NULL,
+           `email1` varchar(100) NOT NULL,
+           `email2` varchar(100) ,
+           `phone_num1` int NOT NULL,
+           `phone_num2` int ,
+           `Street` varchar(100) NOT NULL,
+           `Pincode` int NOT NULL,
+           `City` varchar(100) NOT NULL,
+           `State` varchar(100) NOT NULL,
+           `Website` varchar(100) ,
+           `Verification` varchar(100) NOT NULL DEFAULT 'Not Verified',
+           PRIMARY KEY (`Username`) );
+           
+CREATE TABLE IF NOT EXISTS `Blood_bank_timings` (
+		    `Username` varchar(100) NOT NULL,
+            `Opening_time` time NOT NULL,
+            `Closing_time` time NOT NULL,
+            `Weekday` varchar(50) NOT NULL,
+            PRIMARY KEY (`Username` , `Weekday`), 
+            FOREIGN KEY (`Username`) REFERENCES `Blood_bank`(`Username`));
+            
+CREATE TABLE IF NOT EXISTS `Blood_stock` (
+            `Id` int NOT NULL AUTO_INCREMENT,
+            `Username` varchar(100) NOT NULL,
+            `Blood_group` varchar(10) NOT NULL,
+            `Adding_date` date ,
+            `Removing_date` date,
+            `Units_added` int,
+            `Units_removed` int,
+            PRIMARY KEY (`Id`),
+            FOREIGN KEY (`Username`) REFERENCES `Blood_bank`(`Username`)
+            ) AUTO_INCREMENT=1;
+            
+  CREATE TABLE IF NOT EXISTS `Donor` (
+              `Email_id` varchar(100) NOT NULL,
+              `Password` varchar(100) NOT NULL ,
+              `First_name` varchar(100) NOT NULL,
+              `Last_name` varchar(100) ,
+              `Age` int NOT NULL,
+              `Gender` varchar(30) NOT NULL,
+              `Blood_group` varchar(20) NOT NULL,
+              `Eligibility` bool NOT NULL,
+              `Phone_num` int NOT NULL,
+              `Street` varchar(100) NOT NULL,
+              `Pincode` int NOT NULL,
+              `State` varchar(100) NOT NULL,
+              PRIMARY KEY (`Email_id`) );
+
+CREATE TABLE IF NOT EXISTS `Donations` (
+		    `Email_id` varchar(100) NOT NULL,
+            `Date` date NOT NULL,
+            `Units` int NOT NULL,
+            `Blood_group` varchar(10) NOT NULL,
+            PRIMARY KEY (`Email_id`),
+            FOREIGN KEY (`Email_id`) REFERENCES `Donor`(`Email_id`) );
+
+CREATE TABLE IF NOT EXISTS `Notif_Admin_to_Blood_bank` (
+            `Id` int NOT NULL,
+            `Username` varchar(100) NOT NULL,
+            `Message` varchar(500) NOT NULL,
+            `Date` date NOT NULL,
+            PRIMARY KEY (`Id`),
+            FOREIGN KEY (`Username`) REFERENCES `Blood_bank`(`Username`) );
+            
+CREATE TABLE IF NOT EXISTS `Notif_Blood_bank_to_Donor` (
+            `Id` int NOT NULL,
+            `Blood_bank_user` varchar(100) NOT NULL,
+            `Donor_user` varchar(100) NOT NULL,
+            `Message` varchar(500) NOT NULL,
+            `Date` date NOT NULL,
+            PRIMARY KEY (`Id`),
+            FOREIGN KEY (`Blood_bank_user`) REFERENCES `Blood_bank`(`Username`),
+			FOREIGN KEY (`Donor_user`) REFERENCES `Donor`(`Email_id`));
+            
+CREATE TABLE IF NOT EXISTS `Notif_Admin_to_Donor` (
+            `Id` int NOT NULL,
+            `Donor_user` varchar(100) NOT NULL,
+            `Message` varchar(500) NOT NULL,
+            `Date` date NOT NULL,
+            PRIMARY KEY (`Id`),
+            FOREIGN KEY (`Donor_user`) REFERENCES `Donor`(`Email_id`) );
+            
+CREATE TABLE IF NOT EXISTS `Message_to_admin` (
+            `Id` int NOT NULL,
+            `Name` varchar(100) NOT NULL,
+            `Email_id` varchar(100) NOT NULL,
+            `phone_num` int NOT NULL,
+            `Message` varchar(500) NOT NULL,
+            `Date` date NOT NULL,
+            PRIMARY KEY (`Id`) );
+            
+ CREATE TABLE IF NOT EXISTS `Post_by_admin` (
+            `Id` int NOT NULL,
+            `Heading` varchar(200) NOT NULL,
+            `Post` varchar(700) NOT NULL,
+            `Date` date NOT NULL,
+            PRIMARY KEY (`Id`) );
---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask ,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/user_login")
+def user_login():
+    return render_template('user_login.html')
+
+@app.route("/user_signup")
+def user_signup():
+    return render_template('user_signup.html')
+
+@app.route("/blood_bank_registeration")
+def blood_bank_registeration():
+    return render_template('blood_bank_regis_me.html')
+
+@app.route("/blood_bank_login")
+def blood_bank_login():
+    return render_template('blood_bank_login.html')
+
+@app.route("/admin_dashboard")
+def admin_dashboard():
+    return render_template('admin_dashboard.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+app.run(debug=True)
